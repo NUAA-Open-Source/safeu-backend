325b3bb6f874ca5d1b959818f536f202e4d3f33e
Triple-Z <me@triplez.cn>
2019-02-04 15:46:32 +0800

    Fix #10 review suggestions from Guobin Wang

--- validation/validationController.go
+++ validation/validationController.go
@@ -34,17 +34,17 @@ func GenerateTokenByMd5() string {
 
 func Validation(c *gin.Context) {
 	retrieveCode := c.Param("retrieveCode")
-	log.Println(c.ClientIP() + " Received validation request for " + retrieveCode + " resources")
+	log.Println(c.ClientIP(), " Received validation request for ", retrieveCode, " resources")
 
 	db := common.GetDB()
 
 	// 是否存在文件
 	var curItem item.Item
-	if db.Where("re_code = ? AND (status = 2 OR status = 3)", retrieveCode).First(&curItem).RecordNotFound() {
+	if db.Where("re_code = ? AND (status = ? OR status = ?)", retrieveCode, common.UPLOAD_FINISHED, common.FILE_ACTIVE).First(&curItem).RecordNotFound() {
 		c.JSON(http.StatusNotFound, gin.H{
 			"error": "Cannot find resource via this retrieve code.",
 		})
-		log.Println(c.ClientIP() + " Cannot find resource via the retrieve code " + retrieveCode)
+		log.Println(c.ClientIP(), " Cannot find resource via the retrieve code ", retrieveCode)
 		return
 	}
 
@@ -53,11 +53,11 @@ func Validation(c *gin.Context) {
 		// 文件公有，生成 Token 并返回文件列表
 		token := GenerateTokenByMd5()
 		var itemList []item.Item
-		db.Where("re_code = ? AND (status = 2 OR status = 3)", retrieveCode).Find(&itemList)
+		db.Where("re_code = ? AND (status = ? OR status = ?)", retrieveCode, common.UPLOAD_FINISHED, common.FILE_ACTIVE).Find(&itemList)
 
-		tokenRecord := Token{Token: token, RetrieveCode: retrieveCode, Valid: true, ExpiredAt: time.Now().Add(15 * time.Minute)} // Token 有效期为 15 min
+		tokenRecord := Token{Token: token, RetrieveCode: retrieveCode, Valid: true, ExpiredAt: time.Now().Add((time.Duration)(common.TOKEN_VALID_MINUTES) * time.Minute)} // Token 有效期
 		db.Create(&tokenRecord)
-		log.Println(c.ClientIP() + " Generated token " + token + " for retrieve code " + retrieveCode)
+		log.Println(c.ClientIP(), " Generated token ", token, " for retrieve code ", retrieveCode)
 
 		c.JSON(http.StatusOK, gin.H{
 			"token": token,
@@ -67,13 +67,13 @@ func Validation(c *gin.Context) {
 	}
 
 	// 验证密码
-	// 密码加密方式：SHA256( retrieveCode + SHA256(password) )
+	// 密码加密方式：SHA256( retrieveCode, SHA256(password) )
 	var valiPass ValiPass
 	if err := c.ShouldBindJSON(&valiPass); err != nil {
 		c.JSON(http.StatusUnauthorized, gin.H{
 			"error": "Cannot get the password",
 		})
-		log.Println(c.ClientIP() + " Cannot get the password from client, return 401 Unauthorized")
+		log.Println(c.ClientIP(), " Cannot get the password from client, return 401 Unauthorized")
 		return
 	}
 
@@ -83,27 +83,27 @@ func Validation(c *gin.Context) {
 	hasher.Write([]byte(retrieveCode + SHA256Password))
 	hasherSum := hex.EncodeToString(hasher.Sum(nil))
 
-	//log.Println("[DEBUG] refPassword = " + refPassword)
-	//log.Println("[DEBUG] password = " + SHA256Password)
-	//log.Println("[DEBUG] hasher sum = " + hasherSum)
+	//log.Println("[DEBUG] refPassword = ", refPassword)
+	//log.Println("[DEBUG] password = ", SHA256Password)
+	//log.Println("[DEBUG] hasher sum = ", hasherSum)
 
 	// 密码不正确/密码缺失返回 401
 	if hasherSum != refPassword {
 		c.JSON(http.StatusUnauthorized, gin.H{
 			"error": "The password is not correct",
 		})
-		log.Println(c.ClientIP() + " The password is not correct, return 401 Unauthorized")
+		log.Println(c.ClientIP(), " The password is not correct, return 401 Unauthorized")
 		return
 	}
 
 	// 密码正确生成 Token 并返回文件列表
 	token := GenerateTokenByMd5()
 	var itemList []item.Item
-	db.Where("re_code = ? AND (status = 2 OR status = 3)", retrieveCode).Find(&itemList)
+	db.Where("re_code = ? AND (status = ? OR status = ?)", retrieveCode, common.UPLOAD_FINISHED, common.FILE_ACTIVE).Find(&itemList)
 
-	tokenRecord := Token{Token: token, RetrieveCode: retrieveCode, Valid: true, ExpiredAt: time.Now().Add(15 * time.Minute)} // Token 有效期为 15 min
+	tokenRecord := Token{Token: token, RetrieveCode: retrieveCode, Valid: true, ExpiredAt: time.Now().Add((time.Duration)(common.TOKEN_VALID_MINUTES) * time.Minute)} // Token 有效期
 	db.Create(&tokenRecord)
-	log.Println(c.ClientIP() + " Generated token " + token + " for retrieve code " + retrieveCode)
+	log.Println(c.ClientIP(), " Generated token ", token, " for retrieve code ", retrieveCode)
 
 	c.JSON(http.StatusOK, gin.H{
 		"token": token,
