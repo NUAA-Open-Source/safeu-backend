52b0370b5be86d4263c83b7328884c698802dce7
TripleZ <me@triplez.cn>
2019-02-17 22:58:43 +0800

    Change USTC mirror to DaoCloud mirror & Fix bug

--- scripts/install-docker-family-on-ubuntu-1804.sh
+++ scripts/install-docker-family-on-ubuntu-1804.sh
@@ -1,26 +1,37 @@
 #!/bin/sh
 
+# remove older versions
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+
 # Install docker via USTC mirror
-sudo apt-get update
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+# sudo apt-get update -y
+# sudo apt-get install -y \
+#     apt-transport-https \
+#     ca-certificates \
+#     curl \
+#     gnupg-agent \
+#     software-properties-common
 
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+# curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -y -
 
+# sudo add-apt-repository -y \
+#    "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu \
+#    $(lsb_release -cs) \
+#    stable"
+
+# sudo apt-get update -y
+# sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+# Install docker via DaoCloud mirror
+curl -sSL https://get.daocloud.io/docker | sh
+
+if [ ! -f "/etc/docker/daemon.json" ]; then
 echo "{
-       "registry-mirrors": ["https://docker.mirrors.ustc.edu.cn/"]
-}
+    \"registry-mirrors\": [\"https://docker.mirrors.ustc.edu.cn/\"]
+}   
 " >> /etc/docker/daemon.json
+fi
+
 sudo systemctl restart docker
 
 # Install docker compose via DaoCloud
