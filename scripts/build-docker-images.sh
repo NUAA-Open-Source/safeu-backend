4edf3c98d8b742b83766552bc4721add83279a33
TripleZ <me@triplez.cn>
2019-02-24 18:22:00 +0800

    Add production docker compose file

--- scripts/build-docker-images.sh
+++ scripts/build-docker-images.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-# Build safeu-backend-dev docker image
+# Build safeu-backend-dev docker image for development
 sudo docker build -t safeu-backend-dev:latest -f ../build/package/safeu-backend-dev/Dockerfile ..
+
+# Build safeu-backend docker image for production
+sudo docker build -t safeu-backend:latest -f ../build/package/safeu-backend/Dockerfile ..
