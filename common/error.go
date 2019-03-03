49cc23a33583463c9fa7d3d4a1322efd71899a5e
TripleZ <me@triplez.cn>
2019-03-03 14:11:29 +0800

    Add response error code in update

--- common/error.go
+++ common/error.go
@@ -3,4 +3,8 @@ package common
 var Errors = map[int]string{
 	0: "General error.",
 	1: "Parameter error.",
+	2: "The length of the time is not within the right range.",
+	3: "Can't Find User Token.",
+	4: "Repeat retrieve code.",
+	5: "Auth field required.",
 }
